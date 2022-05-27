b8b40378499f5f0667a62e2782d239d8414a90af
Zia Hellman <ziahellman@gmail.com>
2022-05-27 17:10:08 +0300

    commit tree b101bde059747b2c1ac1cc955c30d8ecb1597f83

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "mary";
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "not bob";
+}
